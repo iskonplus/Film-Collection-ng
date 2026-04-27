78e5809a1914e345386431bf69a99bc08760c387
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-04-27 22:01:01 +0200

    feat add not found page

--- film-collection-ng/src/app/pages/not-found/not-found.ts
+++ film-collection-ng/src/app/pages/not-found/not-found.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './not-found.html',
   styleUrl: './not-found.scss',
 })
-export class NotFound {}
+export class NotFound {
+  title = signal("Page not found");
+}
