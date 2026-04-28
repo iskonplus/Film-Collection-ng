ebc2fb5a31a2455259f37b8b1b944ae475b5f65d
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-04-28 19:40:33 +0200

    feat: add navigation to film details page via routerLink in film card

--- film-collection-ng/src/app/shared/components/film-card/film-card.ts
+++ film-collection-ng/src/app/shared/components/film-card/film-card.ts
@@ -1,24 +1,19 @@
 import { Component, input, output } from '@angular/core';
 import { Film } from '../../../types/film.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-film-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './film-card.html',
   styleUrl: './film-card.scss',
 })
 export class FilmCard {
   readonly film = input.required<Film>();
   readonly favoriteToggled = output<number>();
-  readonly cardClicked = output<number>();
 
-  onToggleFavorite(): void {
+  onToggleFavorite(event: MouseEvent): void {
+    event.stopPropagation();
     this.favoriteToggled.emit(this.film().id);
-
-  }
-
-  onCardClick(): void {
-    this.cardClicked.emit(this.film().id);
-
   }
 }
