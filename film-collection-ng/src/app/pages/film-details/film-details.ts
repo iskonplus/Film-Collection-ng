16d9c5376e064be79e28b2673e64afefcce49407
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-04-28 21:13:24 +0200

    feat: add custom pipe duration

--- film-collection-ng/src/app/pages/film-details/film-details.ts
+++ film-collection-ng/src/app/pages/film-details/film-details.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FilmService } from '../../shared/services/film.service';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { DurationPipe } from '../../shared/pipes/duration-pipe';
 
 @Component({
   selector: 'app-film-details',
-  imports: [RouterLink],
+  imports: [RouterLink, DurationPipe],
   templateUrl: './film-details.html',
   styleUrl: './film-details.scss',
 })
