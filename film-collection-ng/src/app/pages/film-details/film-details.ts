be9cd7b6c838d2c51794dab9e61966a51a5aa574
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-04-28 20:33:43 +0200

    feat: add film details page logic, temlate, style

--- film-collection-ng/src/app/pages/film-details/film-details.ts
+++ film-collection-ng/src/app/pages/film-details/film-details.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FilmService } from '../../shared/services/film.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-film-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './film-details.html',
   styleUrl: './film-details.scss',
 })
-export class FilmDetails {}
+export class FilmDetails {
+  private readonly filmService = inject(FilmService);
+  private readonly route = inject(ActivatedRoute);
+
+    readonly film = this.filmService.getFilmById(
+    Number(this.route.snapshot.paramMap.get('id'))
+  );
+}
