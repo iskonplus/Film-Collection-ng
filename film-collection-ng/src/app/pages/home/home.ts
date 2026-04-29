41d09c8d1c4e35d807b2c753bf74a8e147fa2949
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-04-29 13:31:56 +0200

    feat: add film filtering by search input

--- film-collection-ng/src/app/pages/home/home.ts
+++ film-collection-ng/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { FilmCard } from '../../shared/components/film-card/film-card';
 import { FilmService } from '../../shared/services/film.service';
 
@@ -10,8 +10,22 @@ import { FilmService } from '../../shared/services/film.service';
 })
 export class Home {
   filmService = inject(FilmService);
+  searchValue = signal<string>('');
 
   favoriteToggled(id: number): void {
     this.filmService.toggleFavorite(id);
   }
+
+  onSearch(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.searchValue.set(input.value);
+  }
+
+  filteredFilms = computed(() => {
+    const value = this.searchValue().trim().toLocaleLowerCase();
+    
+    return this.filmService.films().filter(film => {
+      return film.title.toLowerCase().includes(value);
+    })
+  })
 }
